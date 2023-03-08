c08171d0c79ce9a38f6bf7a3b3edc81c11d88fe9
Oleg <hellreebok@gmail.com>
2023-03-08 22:29:20 +0200

    update deploy

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
     <React.StrictMode>
-        <BrowserRouter basename="/poshtaAPI">
+        <BrowserRouter basename="/poshta">
             <App />
         </BrowserRouter>
     </React.StrictMode>
