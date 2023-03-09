d7c0732cbb1723bf39e5fc14d739175bb96991a8
Oleg <hellreebok@gmail.com>
2023-03-09 09:54:18 +0200

    update base structure

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,18 +1,23 @@
 import { FC, Suspense, lazy } from "react";
 import { Route, Routes } from "react-router-dom";
-import { Loader, Main, BranchesList, BranchInfo } from "components";
+import { Header, Loader, Main, BranchesList, BranchInfo } from "components";
 
 const PageNotFound = lazy(() => import("pages/PageNotFound/PageNotFound"));
 
 export const App: FC = () => {
     return (
-        <Suspense fallback={<Loader />}>
-            <Routes>
-                <Route path="/" element={<Main />} />
-                <Route path="branches" element={<BranchesList />} />
-                <Route path="branches/:id" element={<BranchInfo />} />
-                <Route path="*" element={<PageNotFound />} />
-            </Routes>
-        </Suspense>
+        <>
+            <Header />
+            <main className="main">
+                <Suspense fallback={<Loader />}>
+                    <Routes>
+                        <Route path="/" element={<Main />} />
+                        <Route path="branches" element={<BranchesList />} />
+                        <Route path="branches/:id" element={<BranchInfo />} />
+                        <Route path="*" element={<PageNotFound />} />
+                    </Routes>
+                </Suspense>
+            </main>
+        </>
     );
 };
