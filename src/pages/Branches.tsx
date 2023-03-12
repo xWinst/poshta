2a828597067a858d72c99946afbc78ddf0629cf6
Oleg <hellreebok@gmail.com>
2023-03-12 18:55:04 +0200

    update styles

--- src/pages/Branches.tsx
+++ src/pages/Branches.tsx
@@ -1,12 +1,16 @@
-import { BranchesForm, BranchesFilter, BranchesList } from "components";
+import { BranchesForm, BranchesFilter, BranchesList, Loader } from "components";
+import { useAppSelector } from "hooks/reduxHooks";
 import { FC } from "react";
 
 const Branches: FC = () => {
+    const isLoading = useAppSelector((state) => state.branches.isLoading);
+
     return (
         <>
             <BranchesForm />
             <BranchesFilter />
             <BranchesList />
+            {isLoading && <Loader />}
         </>
     );
 };
