7591a834b8c963d846fc71f31dc6f35957e99dca
Oleg <hellreebok@gmail.com>
2023-03-12 08:31:10 +0200

    restructuring project

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,3 @@
-export { default as Branches } from "./Branches/Branches";
-export { default as PageNotFound } from "./PageNotFound/PageNotFound";
+export { default as Main } from "./Main";
+export { default as Branches } from "./Branches";
+export { default as PageNotFound } from "./PageNotFound";
